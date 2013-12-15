b3043d471e2fa74c73f9409907e79e7b5e9bd29e
shirong250 <shirong250@163.com>
2013-12-15 21:40:43 +0800

    Update cli_setting.h

--- include/cli_setting.h
+++ include/cli_setting.h
@@ -1,19 +1,19 @@
 #ifndef _CGI_SETTING_H
 #define _CGI_SETTING_H
 
-#define CLI_Y 1
-#define CLI_N 2
+#define LIBED_Y 1
+#define LIBED_N 2
 
-#define CLI_TEST_SINGLE_CMD
+#define LIBED_TEST_SINGLE_CMD
 
-typedef struct cli_setting_t
+typedef struct libed_setting_t
 {
     int y_or_n;
     int quite;
-} CLI_SETTING;
+} LIBED_SETTING;
 
-int cli_setting_auto_reply_set(struct cli_setting_t *setting);
+int libed_setting_auto_reply_set(struct libed_setting_t *setting);
 
-void cli_setting_usage_print();
+void libed_setting_usage_print();
 
 #endif
